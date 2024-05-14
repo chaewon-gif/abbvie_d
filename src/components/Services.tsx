3ce7233fbc638d03a04036acdae55d897cd4d8df
Son Chaewon <good6637@naver.com>
2024-05-14 11:05:47 +0900

    index

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -17,7 +17,10 @@ function Services() {
         <div className="flex flex-col items-center gap-y-12 lg:flex-row lg:justify-between lg:px-[136px]">
           {services.skills.map((service, index) => {
             return (
-              <div className="p-[30px] w-full max-w-[417px] flex flex-col text-center lg:text-left hover:bg-white hover:shadow-2xl cursor-crosshair transition-all">
+              <div
+                className="p-[30px] w-full max-w-[417px] flex flex-col text-center lg:text-left hover:bg-white hover:shadow-2xl cursor-crosshair transition-all"
+                key={index}
+              >
                 {/* icon */}
                 <div className="w-20 h-20 mb-12 mx-auto lg:mx-0">
                   <img
